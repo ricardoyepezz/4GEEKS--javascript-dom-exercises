1e3c65b712676a22c00a1d8c679215db193f582b
ricardoyepezz <ricardoyepezz@gmail.com>
2022-03-02 21:05:26 +0000

    exercise 11 done

--- exercises/11-Add-li-on-Click/index.js
+++ exercises/11-Add-li-on-Click/index.js
@@ -1,5 +1,10 @@
 let button = document.querySelector("#superDuperButton");
 button.addEventListener("click", function() {
 	//your code here
-
+	const li = document.createElement("li");
+	const ul = document.querySelector("#myList")
+	//li.innerHTML= "Fourth element";
+    li.textContent = "Fourth element";
+	ul.appendChild(li);
+	
 });
