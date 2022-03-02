84cd650eee9e8b0c9ca4bd19ce99f662cdf04384
ricardoyepezz <ricardoyepezz@gmail.com>
2022-03-02 20:51:04 +0000

    exercise 10 done

--- exercises/10-Render-on-Click/index.js
+++ exercises/10-Render-on-Click/index.js
@@ -1,5 +1,9 @@
 let button = document.querySelector("#superDuperButton");
 button.addEventListener("click", function() {
 	//your code here
+	const container = document.createElement("div")
+	container.innerHTML= "Hello World";
+    container.style.background = 'yellow'
+	document.body.appendChild(container);
 	
 });
