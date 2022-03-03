014483de6fd8f56b8032ecb1f134b8e85915cca7
ricardoyepezz <ricardoyepezz@gmail.com>
2022-03-03 16:39:48 +0000

    fixes

--- exercises/13-Add-Options-to-the-Select/index.js
+++ exercises/13-Add-Options-to-the-Select/index.js
@@ -1,2 +1,16 @@
 let countries = ["USA", "France", "Italy", "Brazil", "Colombia", "Belize", "Venezuela"];
-// your code here
+    //obtengo elementos de array countries
+countries.forEach(element => {
+    //creo elemento option
+    let option = document.createElement("option")
+
+    //llamo a elemento mySelect para agregar appendChild
+    const select = document.querySelector("#mySelect")
+    select.appendChild(option)
+    option.innerHTML=element;
+    console.log(select);
+   select.addEventListener('click', function() {
+        console.log('hola');
+      });
+
+})
--- exercises/14-Todo-List/index.js
+++ exercises/14-Todo-List/index.js
@@ -1,1 +1,42 @@
 // Your code here
+const input = document.querySelector("input");
+const ul = document.querySelector("ul");
+const empty = document.querySelector(".empty");
+
+//agrego un event listener al boton
+input.addEventListener("keypress", e => {
+  //declaro text igual a la entrada del input
+  const text = input.value;
+  if (text !== "" && e.key === "Enter") {
+    // se añade preventDefault para evitar recargo de pag
+    e.preventDefault();
+    //declaro li para crear elemento
+    const li = document.createElement("li");
+    li.className = "list-group-item";
+    li.textContent = text;
+    //appendChild de li y ul
+    li.appendChild(addDeleteBtn());
+    ul.appendChild(li);
+
+    //coloco input vacio despues de agregar tarea
+    input.value = "";
+    //al detectar contenido en input desaparece div Empty
+    empty.style.display = "none";
+
+  }
+});
+
+function addDeleteBtn() {
+  //creo boton para eliminar
+  const deleteBtn = document.querySelector("li");
+  //caracteristicas de boton eliminar (Contenido y estilo)
+  
+  deleteBtn.addEventListener("click", e => {
+    //constante para llamar a elemento hijo (item)
+    console.log("hola");
+    const item = e.target.parentElement;
+    //elimino elemento (item)
+    ul.removeChild(item);
+  });
+  return deleteBtn;
+}
